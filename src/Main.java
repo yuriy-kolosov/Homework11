15533b2e989746e501a6005de32967fd0c0281b4
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-01-11 16:12:46 +0300

    Домашнее задание 11 - Задачи 1...3 - Редакция 2а

--- src/Main.java
+++ src/Main.java
@@ -94,32 +94,44 @@ public class Main {
         deliveryDistance = 5;
         System.out.println("Расстояние (км) = " + deliveryDistance);
         deliveryTime = checkDistance(deliveryDistance);
-        if (deliveryTime > 0) System.out.println("Время доставки (сутки) = " + deliveryTime);
-        else System.out.println("Доставки нет");
+        if (deliveryTime > 0) {
+            System.out.println("Время доставки (сутки) = " + deliveryTime);
+        } else {
+            System.out.println("Доставки нет");
+        }
 //
         System.out.println();
         System.out.println("        Проверка 11-3-2");
         deliveryDistance = 23;
         System.out.println("Расстояние (км) = " + deliveryDistance);
         deliveryTime = checkDistance(deliveryDistance);
-        if (deliveryTime > 0) System.out.println("Время доставки (сутки) = " + deliveryTime);
-        else System.out.println("Доставки нет");
+        if (deliveryTime > 0) {
+            System.out.println("Время доставки (сутки) = " + deliveryTime);
+        } else {
+            System.out.println("Доставки нет");
+        }
 //
         System.out.println();
         System.out.println("        Проверка 11-3-3");
         deliveryDistance = 87;
         System.out.println("Расстояние (км) = " + deliveryDistance);
         deliveryTime = checkDistance(deliveryDistance);
-        if (deliveryTime > 0) System.out.println("Время доставки (сутки) = " + deliveryTime);
-        else System.out.println("Доставки нет");
+        if (deliveryTime > 0) {
+            System.out.println("Время доставки (сутки) = " + deliveryTime);
+        } else {
+            System.out.println("Доставки нет");
+        }
 //
         System.out.println();
         System.out.println("        Проверка 11-3-4");
         deliveryDistance = 102;
         System.out.println("Расстояние (км) = " + deliveryDistance);
         deliveryTime = checkDistance(deliveryDistance);
-        if (deliveryTime > 0) System.out.println("Время доставки (сутки) = " + deliveryTime);
-        else System.out.println("Доставки нет");
+        if (deliveryTime > 0) {
+            System.out.println("Время доставки (сутки) = " + deliveryTime);
+        } else {
+            System.out.println("Доставки нет");
+        }
     }
 
     public static int checkYearBissextile(int y) { //Проверка на високосный год
@@ -157,19 +169,27 @@ public class Main {
         if (tOs == 1) { // Тип Android
             if (iYear >= 2015) {
                 System.out.println("Установите версию приложения для Android по ссылке");
-            } else System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
         } else if (iYear >= 2015) { // Тип iOS
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
     }
 
-    public static int checkDistance(int Dist) { // Проверка расстояния для доставки
+    public static int checkDistance(int dDist) { // Проверка расстояния для доставки
         int dTime = -1;
 //
-        if (Dist <= 100) {
-            if (Dist > 60) dTime = 3;
-            else if (Dist > 20) dTime = 2;
-            else dTime = 1;
+        if (dDist <= 100) {
+            if (dDist > 60) {
+                dTime = 3;
+            } else if (dDist > 20) {
+                dTime = 2;
+            } else {
+                dTime = 1;
+            }
         }
         return dTime; // Время доставки в сутках
     }
